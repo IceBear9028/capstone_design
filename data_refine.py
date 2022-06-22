628da5a907a28a359e0f598e22b1d9655c84d34c
IceBear9028 <tlatms9028@gmail.com>
2022-06-22 14:34:16 +0900

    4 commit
    focus 데이터 수정
    (예전 데이터 -> 새로운데이터로 수정)

--- data_refine.py
+++ data_refine.py
@@ -9,54 +9,61 @@ Created on Fri Jun 17 21:41:35 2022
 import pandas as pd
 import numpy as np
 
+nf_df = pd.read_csv("capstone_design/Non_Focus.csv",encoding = 'cp949')
+yf_df = pd.read_csv("capstone_design/Focus.csv", encoding = 'cp949')
 
 
-nf_df = pd.read_csv("capstone_design/Non_Focus.csv")
-yf_df = pd.read_csv("capstone_design/Focus.csv")
+# ## A. 비집중df 정제
+# #1. []없애는 과정
+# con_nf = nf_df['얼굴각도'].str.replace('[','')
+# con_nf = con_nf.str.replace(']','')
 
-## A. 비집중df 정제
-#1. []없애는 과정
-con_nf = nf_df['얼굴각도'].str.replace('[','')
-con_nf = con_nf.str.replace(']','')
+# #2. 두개의 데이터프레임으로 나누는 과정(,기준으로 나눔)
+# nf_df[['얼굴최저각도','얼굴최고각도']] = pd.DataFrame(con_nf.str.split(', ',1).tolist())
 
-#2. 두개의 데이터프레임으로 나누는 과정(,기준으로 나눔)
-nf_df[['얼굴최저각도','얼굴최고각도']] = pd.DataFrame(con_nf.str.split(', ',1).tolist())
+# #3. 기존 얼굴각도 날린다.
+# nf_df = nf_df.drop(columns = ['얼굴각도'])
 
-#3. 기존 얼굴각도 날린다.
-nf_df = nf_df.drop(columns = ['얼굴각도'])
+# #4. numeric이용, 각도 숫자형으로 변형
+# nf_df['얼굴최저각도'] = pd.to_numeric(nf_df['얼굴최저각도'])
+# nf_df['얼굴최고각도'] = pd.to_numeric(nf_df['얼굴최고각도'])
 
-#4. numeric이용, 각도 숫자형으로 변형
-nf_df['얼굴최저각도'] = pd.to_numeric(nf_df['얼굴최저각도'])
-nf_df['얼굴최고각도'] = pd.to_numeric(nf_df['얼굴최고각도'])
+# ## B. 집중df 정제
+# #1. []없애는 과정
+# con_yf = yf_df['얼굴각도'].str.replace('[','')
+# con_yf = con_yf.str.replace(']','')
 
-## B. 집중df 정제
-#1. []없애는 과정
-con_yf = yf_df['얼굴각도'].str.replace('[','')
-con_yf = con_yf.str.replace(']','')
+# #2. 두개의 데이터프레임으로 나누는 과정(,기준으로 나눔)
+# yf_df[['얼굴최저각도','얼굴최고각도']] = pd.DataFrame(con_yf.str.split(', ',1).tolist())
 
-#2. 두개의 데이터프레임으로 나누는 과정(,기준으로 나눔)
-yf_df[['얼굴최저각도','얼굴최고각도']] = pd.DataFrame(con_yf.str.split(', ',1).tolist())
+# #3. 기존 얼굴각도 날린다.
+# yf_df = yf_df.drop(columns = ['얼굴각도'])
 
-#3. 기존 얼굴각도 날린다.
-yf_df = yf_df.drop(columns = ['얼굴각도'])
+# #4. numeric이용, 각도 숫자형으로 변형
+# yf_df['얼굴최저각도'] = pd.to_numeric(yf_df['얼굴최저각도'])
+# yf_df['얼굴최고각도'] = pd.to_numeric(yf_df['얼굴최고각도'])
 
-#4. numeric이용, 각도 숫자형으로 변형
-yf_df['얼굴최저각도'] = pd.to_numeric(yf_df['얼굴최저각도'])
-yf_df['얼굴최고각도'] = pd.to_numeric(yf_df['얼굴최고각도'])
 
 ## C. 인덱스 부착,두 df 합침
 nf_df["focus"] = 0
 yf_df["focus"] = 1
 
+
 df = pd.concat([yf_df, nf_df])
+df = df.reset_index(drop = True)
+df['얼굴각도'] = df['얼굴각도'].round(3)
+
 
 ## D. 얼굴각도 차이 계산, 최종df 완성
-angle_dif = df['얼굴최고각도'] - df['얼굴최저각도']
-angle_dif = angle_dif.round(3)
-df.insert(12,'얼굴각도차이', angle_dif)
+# angle_dif = df['얼굴최고각도'] - df['얼굴최저각도']
+# angle_dif = angle_dif.round(3)
+# df.insert(12,'얼굴각도차이', angle_dif)
+
+
 
 ## E. 최종df 추출
 df.to_csv("capstone_design/Focus_concat.csv")
+
 
 df_sad = pd.concat([df['슬픔'],df['focus']],axis = 1)
 df_sad = df_sad.reset_index(drop = True)
--- data_visualize_dash.py
+++ data_visualize_dash.py
@@ -14,32 +14,54 @@ server = Flask(__name__)
 app = Dash(__name__, server = server)
 
 # -> dash를 app 오브젝트로 저장함.
-graph_id = 'graph_input'
+graph_id_1= 'graph_focus'
+graph_id_2= 'grapf_nofocus'
 input_id_1 = 'columns_name'
 
+# 색깔 지정가능
+colors = {
+    text_1 : '#7FDBFF'
+}
 
 app.layout = html.Div([
     #1. 그래프 그리는 코어 생성
-    dcc.Graph(id=graph_id),
+    html.Div([
+        "Grapf-Focus",
+        dcc.Graph(id=graph_id_1)]),
+    html.Div([
+        "Graph-Non_Focus",
+        dcc.Graph(id=graph_id_2)]),
 
     #2. 그래프 input 객체 코어 생성
-    dcc.Dropdown(
+    html.Div([
+        dcc.Dropdown(
         df.columns,
-        id = input_id_1
-    )
-])
+        id = input_id_1)])
+    ])
 
 @app.callback(
-    Output(graph_id,"figure"),
+    Output(graph_id_1,"figure"),
     Input(input_id_1,"value")
     ) # <- 아래 update_figure의 변수로 들어감(input_1으로)
     
 def update_figure(input_1):
     #여기다가 그래프 객체 선언함.
     #fig = go.Figure(data = [go.Box(y = filtered_df, boxpoints = 'all')])
-    fig = px.box(df, x = input_1)
+    filter_df_1 = df[df['focus']==0]
+    fig_1 = px.box(filter_df_1, x = input_1)
+    return fig_1
 
-    return fig
+@app.callback(
+    Output(graph_id_2,"figure"),
+    Input(input_id_1,"value")
+    )
+
+def update_figure(input_2):
+    #여기다가 그래프 객체 선언함.
+    #fig = go.Figure(data = [go.Box(y = filtered_df, boxpoints = 'all')])
+    filter_df_2 = df[df['focus']==1]
+    fig_2 = px.box(filter_df_2, x = input_2)
+    return fig_2
 
 if __name__ == '__main__':
     app.run_server(debug=True)
