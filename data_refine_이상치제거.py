90b46e624736abca97bc051bc27cd2dc8d9a4909
IceBear9028 <tlatms9028@gmail.com>
2022-06-29 15:21:15 +0900

    9 commit
    1. data 이상치 제거 코드 변경
    -> 파라미터(척도)넣으면 알아서 이상치 제거, csv파일까지 나오게 한다.
    
    2. 위 코드로 4가지 척도(움직임, 움직임z_X,얼굴각도, 중앙 벗어난 정도)에 대한 csv 파일 생성

--- data_refine_이상치제거.py
+++ data_refine_이상치제거.py
@@ -31,60 +31,77 @@ yf_df = pd.read_csv("capstone_design/Focus.csv", encoding = 'cp949')
 ###############
 #%%
 #집중하지 않은 데이터 이상치 제거 코드
-quartile_1 = nf_df.quantile(0.25)
-quartile_3 = nf_df.quantile(0.75)
+# quartile_1 = nf_df.quantile(0.25)
+# quartile_3 = nf_df.quantile(0.75)
+
+# iqr = quartile_3 - quartile_1
+
+# condition = (nf_df < (quartile_1 - 1.5 * IQR)) | (nf_df > (quartile_3 + 1.5 * IQR))
+# condition = condition.any(axis=1)
+
+# refine_df_nf = nf_df[~condition]
+# refine_df_nf['focus'] = 0
+# #%%
+# #집중한 데이터의 이상치 제거 코드
+# quartile_1 = yf_df.quantile(0.25)
+# quartile_3 = yf_df.quantile(0.75)
+
+# iqr = quartile_3 - quartile_1
+
+# condition = (yf_df < (quartile_1 - 1.5 * IQR)) | (yf_df > (quartile_3 + 1.5 * IQR))
+# condition = condition.any(axis=1)
+
+# refine_df_yf = yf_df[~condition]
+# refine_df_yf['focus'] = 1
+
+
+#%%
+#1단계 : 집중하지 않은 데이터의 'a(척도)'기준 이상치 제거
+a = '중앙에서벗어난정도'  # <<<<<< a에 척도 적으면 알아서 df까지 만들어줌
+
+quartile_1 = nf_df[a].quantile(0.25)
+quartile_3 = nf_df[a].quantile(0.75)
 
 iqr = quartile_3 - quartile_1
 
-condition = (nf_df < (quartile_1 - 1.5 * IQR)) | (nf_df > (quartile_3 + 1.5 * IQR))
-condition = condition.any(axis=1)
+condition = (nf_df[a] < (quartile_1 - 1.5 * iqr)) | (nf_df[a] > (quartile_3 + 1.5 * iqr))
 
 refine_df_nf = nf_df[~condition]
-refine_df_nf['focus'] = 0
-#%%
-#집중하지 않은 데이터의 '슬픔'기준 이상치 제거
-quartile_1 = nf_df['화남'].quantile(0.25)
-quartile_3 = nf_df['화남'].quantile(0.75)
 
-iqr = quartile_3 - quartile_1
-
-condition = (nf_df['화남'] < (quartile_1 - 1.5 * iqr)) | (nf_df['화남'] > (quartile_3 + 1.5 * iqr))
-
-refine_df_nf = nf_df[~condition]
-condition.value_counts()
-
-#%%
-#집중한 데이터의 이상치 제거 코드
-quartile_1 = yf_df.quantile(0.25)
-quartile_3 = yf_df.quantile(0.75)
-
-iqr = quartile_3 - quartile_1
-
-condition = (yf_df < (quartile_1 - 1.5 * IQR)) | (yf_df > (quartile_3 + 1.5 * IQR))
-condition = condition.any(axis=1)
-
-refine_df_yf = yf_df[~condition]
-refine_df_yf['focus'] = 1
+# 파라미터 a의 한개의 열만 출력되게 한다.
+refine_df_nf = refine_df_nf[a]
+refine_df_nf = refine_df_nf.reset_index(drop = True)
+refine_df_nf = refine_df_nf.rename('%s_0' %(a))
 
 
 #%%
-#집중한 데이터의 '슬픔' 기준 이상치 제거
-quartile_1 = yf_df['화남'].quantile(0.25)
-quartile_3 = yf_df['화남'].quantile(0.75)
+#2단계 : 집중한 데이터의 'a(척도)' 기준 이상치 제거
+
+quartile_1 = yf_df[a].quantile(0.25)
+quartile_3 = yf_df[a].quantile(0.75)
 
 iqr = quartile_3 - quartile_1
 
-condition = (yf_df['화남'] < (quartile_1 - 1.5 * iqr)) | (yf_df['화남'] > (quartile_3 + 1.5 * iqr))
+condition = (yf_df[a] < (quartile_1 - 1.5 * iqr)) | (yf_df[a] > (quartile_3 + 1.5 * iqr))
 
 refine_df_yf = yf_df[~condition]
 
-condition.value_counts()
+# 파라미터 a의 한개의 열만 출력되게 한다.
+refine_df_yf = refine_df_yf[a]
+refine_df_yf = refine_df_yf.reset_index(drop = True)
+refine_df_yf = refine_df_yf.rename('%s_1' %(a))
 
 
 #%%
-# 집중,비집중 데이터프레임 하나로 합침.
-refine_df = pd.concat([refine_df_yf,refine_df_nf])
+#3단계 : 집중,비집중 데이터프레임 하나로 합침.
+refine_df = pd.concat([refine_df_yf,refine_df_nf],axis =1)
+#그냥 concat 하면 같은 인덱스랑 붙어져서 중간중간 nan값이 나온다.
+
 refine_df = refine_df.reset_index(drop = True)
+#%%
+#4단계 : 집중, 비집중 데이터 csv로 만들어준다.
+refine_df.to_csv("capstone_design/%s_이상치제거concat.csv" %a)
+
 
 #%%
 # 집중, 비집중 데이터 이상치 따로 제거하고 합친 csv파일
@@ -99,7 +116,7 @@ refine_df_nf.to_csv("capstone_design/Non_Focus_refine_by화남.csv")
 refine_df = df[~condition]
 
 #%%
-refine_df.to_csv("capstone_design/Focus_concat_refine.csv")
+refine_df.to_csv("capstone_design/%s_이상치제거concat.csv" %a)
 
 
 #%%
