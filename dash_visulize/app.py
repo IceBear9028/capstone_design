4ed94db605af72e042b7f3b77c32fa0697a36631
IceBear9028 <tlatms9028@gmail.com>
2022-06-30 16:51:21 +0900

    11 commit
    1. plotly dash의 그래프를 css에 적용될 수 있게 app.py 코드 변경
    
    2. 변경한 코드를 css를 이용하여 구간을 나눠서 배치

--- dash_visulize/app.py
+++ dash_visulize/app.py
@@ -14,8 +14,7 @@ server = Flask(__name__)
 app = Dash(__name__, server = server)
 
 # -> dash를 app 오브젝트로 저장함.
-graph_id_1= 'graph_focus'
-graph_id_2= 'grapf_nofocus'
+graph_id_2= 'graph_box'
 graph_id_3= 'graph-scatter_y'
 graph_id_4= 'graph-scatter_N'
 graph_id_5= 'graph-scatter_y_'
@@ -31,83 +30,111 @@ input_id_5 = 'user_name'
 
 # 색깔 지정가능
 
-app.layout = html.Div([
-    #1. 그래프 그리는 코어 생성
-    html.Div([
+app.layout = html.Div(
+    className = "main", 
+    children = [
+    html.Div(
+        className = "box-plot",
+        children = [
         html.H1("Box-plot", className = 'headTitle'),
         html.Div(
             className="box-plot-graph",
-            children = [
-                dcc.Graph(id = graph_id_1), dcc.Graph(id = graph_id_2)]
+            children = [dcc.Graph(id = graph_id_2)]
         ),
         html.Div(
             className = 'box-plot-input',
             children = [html.H3("select category", className = "selectTitle"),
-                        dcc.Dropdown(df.columns,id = input_id_1)]
+                        dcc.Dropdown(df.columns,"중립",id = input_id_1)]
         )
     ]),
 
-    html.Div([
-        html.H1("Scatter-by.person", className = 'headTitle'),
+    html.Div(
+        className = "scatter-plot",
+        children = [
+        html.H1("Scatter-plot(학습자)", className = 'headTitle'),
         html.Div(
-            className = "scatter-plot",
+            className = "scatter-plot-graph",
             children = [
                 html.H2("집중한 사람", className = "subTitle"),
-                dcc.Graph(id = graph_id_3),
+                html.Div(
+                    className = "scatter-plot-graph1",
+                    children = [dcc.Graph(id = graph_id_3)]
+                ),
                 html.H2("집중하지 않은 사람", className = "subTitle"),
-                dcc.Graph(id = graph_id_4),
+                html.Div(
+                    className = "scatter-plot-graph2",
+                    children = [dcc.Graph(id = graph_id_4)]
+                ),
                 html.Div(
                     className = 'scatter-plot-input',
                     children = [html.H3("input", className = "selectTitle"),
-                                dcc.Dropdown(df.columns,id = input_id_2)]
+                                dcc.Dropdown(df.columns,"행복",id = input_id_2)]
                 )
             ]
         )
     ]),
 
+    html.Div([
+        html.Div(
+            className = 'scatter-plot2',
+            children = [
+                html.H1("scatter - 2개 척도별 확인"),
+                html.H2("집중한 사람", className = "subTitle"),
+                html.Div(
+                    className = "scatter-plot2-graph1",
+                    children = [dcc.Graph(id = graph_id_5)]
+                ),
+                html.H2("집중하지 않은 사람", className = "subTitle"),
+                html.Div(
+                    className = "scatter-plot2-graph2",
+                    children = [dcc.Graph(id = graph_id_6)]
+                ),
+                html.Div(
+                    className = "selectTitle",
+                    children = [html.H3("척도 1", className = 'selectTitle'),
+                                dcc.Dropdown(df.columns,"Unnamed: 0",id = input_id_3)]
+                ),
+                html.Div(
+                    className = "selectTitle",
+                    children = [html.H3("척도 2", className = "selectTitle"),
+                                dcc.Dropdown(df.columns,"중립",id = input_id_4)]
+                )
+            ]
+        )
+    ]),
 
     html.Div([
-        html.H1("Scatter-by.person-각 척도별"),
-        html.H2("집중한 사람"),
-        dcc.Graph(id=graph_id_5),
-        html.H2("집중하지 않은 사람"),
-        dcc.Graph(id=graph_id_6),
-        dcc.Dropdown(
-            df.columns,
-            id = input_id_3),
-        dcc.Dropdown(
-            df.columns,
-            id = input_id_4)]),
-
-    html.Div([
-        html.H1("Scatter-by.person-각 척도별"),
-        dcc.Graph(
-            id=graph_id_7),
-        dcc.Dropdown(
-            df['이름'].unique(),
-            id = input_id_5)
-        ])
-
+        html.Div(
+            className = "corr",
+            children = [
+                html.H1("각 학습자간의 척도관계도"),
+                html.Div(
+                    className = "corr-graph",
+                    children = [dcc.Graph(id = graph_id_7)]
+                ),
+                html.Div(
+                    className = "selectTitle",
+                    children = [html.H3("학습자 이름", className = "selectTitle"),
+                                dcc.Dropdown(df['이름'].unique(),"곽민상",id = input_id_5)]
+                )
+            ]
+        )
+    ])
 ])
-
-
 
     #2. 그래프 input 객체 코어 생성
 
 @app.callback(
-    Output(graph_id_1,"figure"),
     Output(graph_id_2,"figure"),
     Input(input_id_1,"value")
     ) # <- 아래 update_figure의 변수로 들어감(input_1으로)
     
 def update_figure_box(input_1):
-    filter_df_1 = df[df['focus']==1]
-    filter_df_2 = df[df['focus']==0]
+    fig_2 = px.box(df, x = 'focus', y = input_1, points = 'all')
 
-    fig_1 = px.box(filter_df_1, x = input_1)
-    fig_2 = px.box(filter_df_2, x = input_1)
+    fig_2.update_layout(height = 1020)
 
-    return fig_1,fig_2
+    return fig_2
 
 @app.callback(
     Output(graph_id_3,"figure"),
@@ -150,8 +177,7 @@ def corr_graph(input):
     filter_df = user_df.drop(['구간순서'], axis = 1)
     
     fig = px.parallel_coordinates(
-        filter_df, color = 'focus', color_continuous_scale=px.colors.diverging.Tealrose
-    )
+        filter_df, color = 'focus',color_continuous_scale=px.colors.diverging.Tealrose)
     return fig
 
 if __name__ == '__main__':
